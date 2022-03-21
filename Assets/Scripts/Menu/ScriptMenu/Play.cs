a5cd196fa769a4f06ede8da4eb2066e989ff61da
monia <moniaw090@gmail.com>
2022-03-21 09:29:08 +0100

    menu1

---
+++ Assets/Scripts/Menu/ScriptMenu/Play.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+   public void Playy ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
