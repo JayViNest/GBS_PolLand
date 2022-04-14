05287f94bda5a72c92a66917cd87a10f14c6bde2
JayVi <Jayvi.nest@gmail.com>
2022-04-14 19:12:15 +0200

    kod wersja 0.6
    Masslo

--- Assets/Scripts/Placeableobject.cs
+++ Assets/Scripts/Placeableobject.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Placeableobject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool Placed { get; private set; }
+    private Vector3Int Size { get; private set; }
+    private Vector3[] Vertices;
+
+    private void GetColliderVertexPostionsLocal()
     {
-        
+        BoxCollider b = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[1] = b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[2] = b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f;
+        Vertices[3] = b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CalculateSizeInCells()
     {
-        
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = TileMapBuildScript.current.gridLayout.WorldToCell(worldPos);
+        }
+
+        Size = new Vector3Int(Math.Abs((vertices[0] - vertices[1]).x), 
+                                Math.Abs((vertices[0] - vertices[3]).y), 
+                                    1);
     }
+
+    private Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
+    
+    
 }
