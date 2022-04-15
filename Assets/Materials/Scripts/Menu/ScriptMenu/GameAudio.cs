9c5f4b24d3afecd4cfd35689f77dc84e2751ebef
monia <moniaw090@gmail.com>
2022-04-15 19:09:20 +0200

    na dole róże

---
+++ Assets/Materials/Scripts/Menu/ScriptMenu/GameAudio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudio : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
---
+++ Assets/Scripts/Menu/ScriptMenu/Wyjdz.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wyjdz : MonoBehaviour
+{
+  public void Wyjdzz()
+    {
+        Application.Quit();
+        Debug.Log("Wyjdü");
+    }
+}
